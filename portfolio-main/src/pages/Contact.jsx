29881641874593ff6f2b0e33e6bbf7d112cbe95b
Jeevapriya14 <jeevapriya1473@gmail.com>
2025-04-14 21:26:25 +0530

    updated

--- portfolio-main/src/pages/Contact.jsx
+++ portfolio-main/src/pages/Contact.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import  { useState } from 'react';
+
 import { Navbar } from '../components/Navbar';
 import { Github, Linkedin, Code } from 'lucide-react';
 
